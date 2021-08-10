b774b818142808fce7d577a782d41cb9afc3d55c
globalworming <andreas.worm@headissue.com>
2021-08-10 21:04:37 +0200

    superflous

--- src/test/kotlin/ActorTest.kt
+++ src/test/kotlin/ActorTest.kt
@@ -6,20 +6,20 @@ class ActorTest {
 
   @Test
   internal fun `an actor exists`() {
-    val actor = Actor(canAnswerQuestions = false)
+    val actor = Actor()
     assertThat(actor, `is`(not(nullValue())))
   }
 
 
   @Test
   internal fun `actor has abilities`() {
-    val actor = Actor(canAnswerQuestions = false)
+    val actor = Actor()
     assertThat(actor.ability, `is`(not(nullValue())))
   }
 
   @Test
   internal fun `actor performs tasks`() {
-    val actor = Actor(canAnswerQuestions = false)
+    val actor = Actor()
     actor.perform(object : Performable {
       override fun perform() {}
     })
@@ -28,7 +28,7 @@ class ActorTest {
 
   @Test
   internal fun `actor asks question`() {
-    val actor = Actor(canAnswerQuestions = false)
+    val actor = Actor()
     val answer = actor.asks(object : Question {
       override fun answer(): Any {
         return true
