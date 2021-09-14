e8b3a7b5c2a2be3d673d5c9e52d6242058e350ba
globalworming <andreas.worm@headissue.com>
2021-09-14 21:54:06 +0200

    try migrating to junit5

--- src/test/kotlin/com/example/e2e/api/JsonPlaceHolderIT.kt
+++ src/test/kotlin/com/example/e2e/api/JsonPlaceHolderIT.kt
@@ -5,18 +5,27 @@ import com.example.screenplay.action.UploadNewPost
 import com.example.screenplay.question.NumberOfReturnedPosts
 import net.serenitybdd.screenplay.Actor
 import net.serenitybdd.screenplay.GivenWhenThen.*
+import net.serenitybdd.screenplay.actors.Cast
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi
 import net.serenitybdd.screenplay.rest.questions.ResponseConsequence.*
 import net.thucydides.core.annotations.Narrative
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers
 import org.hamcrest.Matchers.*
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 
 @Narrative(text = ["a simple REST API example"])
 open class JsonPlaceHolderIT {
+
+  val cast = Cast.whereEveryoneCan(CallAnApi.at("https://jsonplaceholder.typicode.com"))
+  private lateinit var author: Actor
+
+  @BeforeEach
+  internal fun setUp() {
+    author = cast.actorNamed("author")
+  }
 
   @Test
   fun `when there are many posts`() {
@@ -30,16 +39,5 @@ open class JsonPlaceHolderIT {
     author.should(seeThatResponse {
       it.statusCode(HttpStatus.SC_CREATED).body("id", CoreMatchers.`is`(101))
     })
-  }
-
-  companion object {
-    private lateinit var author: Actor
-
-    @JvmStatic
-    @BeforeAll
-    fun setUp() {
-      author = Actor("author")
-      author.can(CallAnApi.at("https://jsonplaceholder.typicode.com"))
-    }
   }
 }
